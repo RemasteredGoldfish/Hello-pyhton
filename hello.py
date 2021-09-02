e030b4bb664630f39e11401a5aadc65b853ec1eb
RemasteredGoldfish <89778483+RemasteredGoldfish@users.noreply.github.com>
2021-09-02 20:59:05 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,25 @@
+print('hello world') #output to terminal
+name = input('What s your name: ') #input from terminal
+print('Hello, ' + name)
+name = (name)
+
+age = input ('How old are you?: ')
+print (age + '? damn your old asf ' + name + '!')
+
+born = input ('what year were you born in? ') 
+x = int(input())
+if x > 2000:
+    print ('youning')
+else:
+    print ('oldy')
+
+
+
+
+
+
+    #if yborn > '2000':
+    #print (born + 'you are not that old...')
+    #else
+    #print (born + 'jeezzzz your old as shit')
+
